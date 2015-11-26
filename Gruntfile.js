d7b29c7ce969df23e43ef0835b4686c632c1f79e
Mike Jolley <mike.jolley@me.com>
2015-11-26 15:15:47 +0000

    min

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,40 +5,7 @@ module.exports = function( grunt ){
 	grunt.initConfig({
 		// setting folder templates
 		dirs: {
-			css: 'assets/css',
 			js: 'assets/js'
-		},
-
-		// Compile all .less files.
-		less: {
-			compile: {
-				options: {
-					// These paths are searched for @imports
-					paths: ['<%= dirs.css %>/']
-				},
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.css %>/',
-					src: [
-						'*.less',
-						'!icons.less',
-						'!mixins.less'
-					],
-					dest: '<%= dirs.css %>/',
-					ext: '.css'
-				}]
-			}
-		},
-
-		// Minify all .css files.
-		cssmin: {
-			minify: {
-				expand: true,
-				cwd: '<%= dirs.css %>/',
-				src: ['*.css'],
-				dest: '<%= dirs.css %>/',
-				ext: '.css'
-			}
 		},
 
 		// Minify .js files.
@@ -46,50 +13,29 @@ module.exports = function( grunt ){
 			options: {
 				preserveComments: 'some'
 			},
-			frontend: {
-				files: [{
-					expand: true,
-					cwd: '<%= dirs.js %>',
-					src: [
-						'*.js',
-						'!*.min.js'
-					],
-					dest: '<%= dirs.js %>',
-					ext: '.min.js'
-				}]
-			},
+			vendor: {
+				files: {
+					'<%= dirs.js %>/jquery.blockUI.min.js': ['<%= dirs.js %>/jquery.blockUI.js'],
+					'<%= dirs.js %>/sidebar-login.min.js': ['<%= dirs.js %>/sidebar-login.js']
+				}
+			}
 		},
 
 		// Watch changes for assets
 		watch: {
-			less: {
-				files: ['<%= dirs.css %>/*.less'],
-				tasks: ['less', 'cssmin'],
-			},
 			js: {
 				files: [
-					'<%= dirs.js %>/*js',
+					'<%= dirs.js %>/*.js',
 					'!<%= dirs.js %>/*.min.js',
 				],
 				tasks: ['uglify']
 			}
-		},	
+		},
 
 		shell: {
 			options: {
 				stdout: true,
 				stderr: true
-			},
-			txpull: {
-				command: [
-					'tx pull -a -f',
-				].join( '&&' )
-			},
-			generatemos: {
-				command: [
-					'cd languages',
-					'for i in *.po; do msgfmt $i -o ${i%%.*}.mo; done'
-				].join( '&&' )
 			},
 			generatepot: {
 				command: [
@@ -97,32 +43,22 @@ module.exports = function( grunt ){
 				].join( '&&' )
 			}
 		},
-		
+
 	});
 
 	// Load NPM tasks to be used here
 	grunt.loadNpmTasks( 'grunt-shell' );
-	grunt.loadNpmTasks( 'grunt-contrib-less' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
 	// Register tasks
 	grunt.registerTask( 'default', [
-		//'less',
-		//'cssmin',
 		'uglify'
 	]);
 
 	// Just an alias for pot file generation
 	grunt.registerTask( 'pot', [
 		'shell:generatepot'
-	]);
-
-	grunt.registerTask( 'dev', [
-		'default',
-		'shell:txpull',
-		'shell:generatemos'
 	]);
 
 };
--- assets/js/jquery.blockUI.js
+++ assets/js/jquery.blockUI.js
@@ -11,6 +11,7 @@
  *
  * Thanks to Amir-Hossein Sobhi for some excellent contributions!
  */
+
 ;(function() {
 /*jshint eqeqeq:false curly:false latedef:false */
 "use strict";
@@ -473,7 +474,7 @@
 
 			var els;
 			if (full) // crazy selector to handle odd field errors in ie6/7
-				els = $(document.body).children().filter('.blockUI').add('body > .blockUI');
+				els = $('body').children().filter('.blockUI').add('body > .blockUI');
 			else
 				els = $el.find('>.blockUI');
 
